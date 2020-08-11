bfc8ee3d95ecbaa6802e236cef4253211e5780c5
miguel-goncvs <miguel.rosa.goncalves@gmail.com>
2020-08-11 09:18:49 -0300

    server api update

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://apidogs.combr/json';
-/*export const API_URL = 'https://wdtheme.wdt.com.br/json';*/
+/*export const API_URL = 'http://apidogs.combr/json';*/
+export const API_URL = 'https://wdtheme.wdt.com.br/json';
 
 export function TOKEN_POST (body) {
     return {
